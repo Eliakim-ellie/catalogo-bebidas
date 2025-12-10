cc45c3db16ceb95384b5d32d45074919d39e768f
Dilan <dilantienda90@gmail.com>
2025-12-10 10:07:43 -0600

    Componente: Agrega la carpeta de componentes y el componente de Header para el proyecto

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+
+export default function Header() {
+    return (
+        <header className="bg-slate-800">
+            <div className="mx-auto container px-6 py-16">
+                <div className="">
+                    <div className="">
+                        <img src="/logo.svg" alt="Logotipo" className="w-32" />
+                    </div>
+                    <nav className="">
+
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
