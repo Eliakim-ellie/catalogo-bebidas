11cadfc791d4b784f282a4d724d79226566574e7
Dilan <dilantienda90@gmail.com>
2025-12-10 10:38:45 -0600

    Funcion: Agrega navegacion al header del proyecto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,29 @@
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
         <header className="bg-slate-800">
             <div className="mx-auto container px-6 py-16">
-                <div className="">
+                <div className="flex justify-between items-center">
                     <div className="">
                         <img src="/logo.svg" alt="Logotipo" className="w-32" />
                     </div>
-                    <nav className="">
-
-                    </nav>
-                </div>
+                    <nav className="flex gap-4">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"}
+                            >
+                            Inicio
+                        </NavLink>
+                        <NavLink
+                            to="/favoritos"
+                            className={({ isActive }) => isActive ? "text-orange-500 uppercase font-bold" : "text-white uppercase font-bold"}
+                        >
+                            Favoritos
+                        </NavLink>
+                </nav>
             </div>
-        </header>
+        </div>
+        </header >
     )
 }
