2f9687907bd9c35c386d10c20a75fe7316956bfa
Dilan <dilantienda90@gmail.com>
2025-12-15 13:42:47 -0600

    Funcion: Itera sobre la respuesta de la api y con el card muestra la información de la consulta por cada receta

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
 import type z from "zod";
-import {CategoriesAPIResponseSchema, DrinksAPIResponseSchema, SearchFilterSchema} from "../schemas/recipe-schema"
+import {CategoriesAPIResponseSchema, DrinkAPIResponseSchema, DrinksAPIResponseSchema, SearchFilterSchema} from "../schemas/recipe-schema"
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 
 export type SearchFilter = z.infer<typeof SearchFilterSchema>;
+
+export type Drink = z.infer<typeof DrinkAPIResponseSchema>;
 
 export type Drinks = z.infer<typeof DrinksAPIResponseSchema>;
