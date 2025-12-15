a89458a7f9089d7225727aad6411a17db215eb9b
Dilan <dilantienda90@gmail.com>
2025-12-15 09:11:28 -0600

    Funcion: Agrega el resultado de api al state slice y también devtools al store para ver el resultado con redux

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,6 +1,10 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 import { createRecipeSlice, type RecipeSliceType } from "./recipeSlice";
 
-export const useAppStore = create<RecipeSliceType>((...a) => ({//...a toma una copia de los argumentos (set, get) y se puede enviar al slice
-    ...createRecipeSlice(...a) //llamar al slice
-}))
+export const useAppStore = create<RecipeSliceType>()(
+  devtools((...a) => ({
+    //...a toma una copia de los argumentos (set, get) y se puede enviar al slice
+    ...createRecipeSlice(...a), //llamar al slice
+  }))
+);
