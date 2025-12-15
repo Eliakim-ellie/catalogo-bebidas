31cdb955ceca23a77c369a70234212364f92ddaa
Dilan <dilantienda90@gmail.com>
2025-12-15 11:40:49 -0600

    Type: Agrega el tipado para la respuesta de la api con zod y el type para el estado global

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,8 @@
 import type z from "zod";
-import {CategoriesAPIResponseSchema} from "../schemas/recipe-schema"
+import {CategoriesAPIResponseSchema, DrinksAPIResponseSchema, SearchFilterSchema} from "../schemas/recipe-schema"
 
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 
+export type SearchFilter = z.infer<typeof SearchFilterSchema>;
+
+export type Drinks = z.infer<typeof DrinksAPIResponseSchema>;
