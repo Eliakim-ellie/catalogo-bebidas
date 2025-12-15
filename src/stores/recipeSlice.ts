524882dac9199fa9d61ed969baac9e03cc35dafc
Dilan <dilantienda90@gmail.com>
2025-12-15 16:11:52 -0600

    Funcion: Agrega el manejo del estado del modal al estado global

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -10,9 +10,11 @@ export type RecipeSliceType = {
   categories: Categories;
   drinks: Drinks;
   selectedRecipe: Recipe;
+  modal: boolean;
   fetchCategories: () => Promise<void>;
   searchRecipes: (searchFilters: SearchFilter) => Promise<void>;
   selectRecipe: (id: Drink["idDrink"]) => Promise<void>;
+  closeModal: () => void;
 };
 
 export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
@@ -23,6 +25,7 @@ export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
     drinks: [],
   },
   selectedRecipe: {} as Recipe,
+  modal: false,
   fetchCategories: async () => {
     const categories = await getCategories();
     set({ categories: categories });
@@ -33,6 +36,12 @@ export const createRecipeSlice: StateCreator<RecipeSliceType> = (set) => ({
   },
   selectRecipe: async (id) => {
     const selectRecipe = await getRecipeById(id);
-    set({selectedRecipe: selectRecipe})
+    set({ selectedRecipe: selectRecipe, modal: true });
   },
+  closeModal: () => {
+    set({
+        modal: false,
+        selectedRecipe: {} as Recipe,
+    })
+  }
 });
