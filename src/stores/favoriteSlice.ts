78f2df2afa358613fc22e8fc0e4200bfd0804bbe
Dilan <dilantienda90@gmail.com>
2025-12-16 09:47:06 -0600

    Funcion: Agrega al state de favoritos la funcion de agregar/eliminar

--- src/stores/favoriteSlice.ts
+++ src/stores/favoriteSlice.ts
@@ -3,8 +3,22 @@ import type { Recipe } from "../types";
 
 export type FavoritesSliceType = {
     favorites: Recipe[],
+    handleClickFavorite: (recipe: Recipe) => void;
+    favoriteExist: (id: Recipe["idDrink"]) => boolean;
 }
 
-export const createFavoriteSlice: StateCreator<FavoritesSliceType> = () =>({
+export const createFavoriteSlice: StateCreator<FavoritesSliceType> = (set, get) =>({
     favorites: [],
+    handleClickFavorite: (recipe) => {
+        // if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)){
+        if(get().favoriteExist(recipe.idDrink)){ //para evitar duplicación del codigo
+            set((state)=>({favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)}))
+        }else {
+            // set({favorites: [...get().favorites, recipe]})//esta es una forma, sin usar state
+            set((state) => ({favorites: [...state.favorites, recipe]})) //otra forma, utilizando el state
+        }
+    },
+    favoriteExist: (id) => {
+        return get().favorites.some(favorite => favorite.idDrink === id);
+    }
 })
