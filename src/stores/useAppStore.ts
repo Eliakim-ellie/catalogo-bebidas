844deedc7489a48bc2f07b8fc1f67c9a01358c41
Dilan <dilantienda90@gmail.com>
2025-12-16 09:19:56 -0600

    Store: Agrega un store para manejar el estado de los favoritos y lo conecta con el store principal (useAppStore)

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,10 +1,12 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { createRecipeSlice, type RecipeSliceType } from "./recipeSlice";
+import { type  FavoritesSliceType, createFavoriteSlice } from "./favoriteSlice";
 
-export const useAppStore = create<RecipeSliceType>()(
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     //...a toma una copia de los argumentos (set, get) y se puede enviar al slice
     ...createRecipeSlice(...a), //llamar al slice
+    ...createFavoriteSlice(...a)
   }))
 );
