76e74c6f98c24e3e41041fe60b896e4662748800
Dilan <dilantienda90@gmail.com>
2025-12-19 08:45:48 -0600

    Store: Agrega el store para manejar el estado global de la generacion de recetas con ia

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,12 +3,14 @@ import { devtools } from "zustand/middleware";
 import { createRecipeSlice, type RecipeSliceType } from "./recipeSlice";
 import { type  FavoritesSliceType, createFavoriteSlice } from "./favoriteSlice";
 import { createNotificationSlice, type NotificationSliceType } from "./notificationSlice";
+import { createAISlice, type AISlice } from "./aiSlice";
 
-export const useAppStore = create<RecipeSliceType & FavoritesSliceType & NotificationSliceType>()(
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(
   devtools((...a) => ({
     //...a toma una copia de los argumentos (set, get) y se puede enviar al slice
     ...createRecipeSlice(...a), //llamar al slice
     ...createFavoriteSlice(...a),
     ...createNotificationSlice(...a),
+    ...createAISlice(...a),
   }))
 );
