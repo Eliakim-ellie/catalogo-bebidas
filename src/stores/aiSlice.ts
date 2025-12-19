f42455f77835c8005002354abc957a102f58702f
Dilan <dilantienda90@gmail.com>
2025-12-19 12:11:42 -0600

    Store: Agrega el store slice para el estado de la peticion de IA, obtiene la respuesta y la setea al estado global

--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -1,9 +1,21 @@
 import type { StateCreator } from "zustand";
+import IAService from "../services/IAService";
 
 export type AISlice = {
-    recipe: string;
-}
+  recipe: string;
+  generateRecipe: (prompt: string) => Promise<void>;
+};
 
-export const createAISlice: StateCreator<AISlice, [], [], AISlice> = () => ({
-    recipe: "",
-})
+export const createAISlice: StateCreator<AISlice, [], [], AISlice> = (set) => ({
+  recipe: "",
+  generateRecipe: async (prompt) => {
+    const data = await IAService.generateRecipe(prompt);
+
+    //iterar en la respuesta del prompt
+    for await (const textPart of data) {
+      set((state) => ({
+        recipe: state.recipe + textPart,
+      }));
+    }
+  },
+});
