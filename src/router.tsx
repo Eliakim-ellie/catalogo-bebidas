05de89d02f09dd806ce94b6434ef83b491df3491
Dilan <dilantienda90@gmail.com>
2025-12-19 08:33:19 -0600

    Performance: Agrega mejora de performance en el renderizado de las páginas de la aplicacion con lazy y suspense de react

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,9 @@
+import {lazy, Suspense} from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import IndexPage from "./views/IndexPage"
-import FavoritesPage from "./views/FavoritesPage"
-import Layout from "./layouts/layout"
+import Layout from "./layouts/Layout"
+
+const FavoritesPage = lazy(() => import(`./views/FavoritesPage`))
 
 //configuracion del router
 export default function AppRouter() {
@@ -10,7 +12,7 @@ export default function AppRouter() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<IndexPage />} index />
-          <Route path="/favoritos" element={<FavoritesPage />} />
+          <Route path="/favoritos" element={<Suspense fallback="Cargando..."> <FavoritesPage /></Suspense>} />
         </Route>
       </Routes>
     </BrowserRouter>
