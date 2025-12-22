c8a572c796d304196429e0aca4c0d4d4d1289884
Dilan <dilantienda90@gmail.com>
2025-12-22 09:12:37 -0600

    Funcion: Agrega el parámetro de system para dar contexto a la IA sobre el comportamiento y la forma de brindar mejores respuestas

--- src/services/IAService.ts
+++ src/services/IAService.ts
@@ -5,7 +5,9 @@ export default {
     async generateRecipe (prompt: string) {
         const result = streamText({
             model: openrouter("meta-llama/llama-3.1-405b-instruct:free"),
-            prompt: prompt
+            prompt: prompt,
+            system: "Eres un bartender que tiene 50 años de experiencia", //instrucciones de contexto
+            // temperature: 0,
         })
         return result.textStream
     }
